df8e60f8d0efcd776bdb8b4ace46e4c44bc926a1
Mike Scarbrough <moonpies27@yahoo.com>
2020-07-03 23:59:06 -0400

    Implemented error handler wrapper

--- libs/routes/index.js
+++ libs/routes/index.js
@@ -3,9 +3,20 @@ const clusters = require('../controllers/clusters')
 
 module.exports = {
   attachRoutes: function (app) {
-    app.get('/clusters', async (req, res) => {
+    app.get('/clusters', catchErrors(async (req, res) => {
       let list = await clusters.getAllSafe()
       res.json(list)
-    })
+    }))
+  }
+}
+
+function catchErrors(handler) {
+  return async (req, res, next) => {
+      try {
+          return await handler(req, res, next)
+      } catch (e) {
+          console.error(e)
+          next(e)
+      }
   }
 }
